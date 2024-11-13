df564fcdb71449df74657d319da160431a90bbec
khalidmk1 <khalidmk1@users.noreply.github.com>
2024-11-13 10:01:20 +0100

    youtube content

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import Footer from "../components/Footer";
+import BestContentComp from "../components/Home/BestContentComp";
 import Commingsoonsection from "../components/Home/Commingsoonsection";
 import PodcastComp from "../components/Home/PodcastComp";
 import ReelsComp from "../components/Home/ReelsComp";
@@ -13,6 +14,8 @@ export default function Home() {
      <ReelsComp/>
      <TitleHead title={'Podcast'}/>
      <PodcastComp />
+     <TitleHead title={'Best Content'}/>
+     <BestContentComp />
      <Footer/>
     </>
   )
