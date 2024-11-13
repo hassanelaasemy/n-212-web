6ed50f880074c223c523e4332d5b76d89b72a403
khalidmk1 <khalidmk1@users.noreply.github.com>
2024-11-13 12:06:53 +0100

    handle the event

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,29 @@
 import Footer from "../components/Footer";
 import BestContentComp from "../components/Home/BestContentComp";
 import Commingsoonsection from "../components/Home/Commingsoonsection";
+import Event from "../components/Home/Event";
 import PodcastComp from "../components/Home/PodcastComp";
 import ReelsComp from "../components/Home/ReelsComp";
+import Speakers from "../components/Home/Speakers";
 import TitleHead from "../components/TitleHead";
 
 export default function Home() {
   return (
     <>
-     <Commingsoonsection />
-     <TitleHead title={'Formation'}/>
-     <ReelsComp/>
-     <TitleHead title={'Podcast'}/>
-     <PodcastComp />
-     <TitleHead title={'Best Content'}/>
-     <BestContentComp />
-     <Footer/>
+      <Commingsoonsection />
+      <TitleHead title={'Formation'} />
+      <ReelsComp />
+      <TitleHead title={'Podcast'} />
+      <PodcastComp />
+      <TitleHead title={'Best Content'} />
+      <BestContentComp />
+      <TitleHead title={'Events'} />
+      <Event />
+      <TitleHead title={'Instructeurs'} />
+      <Speakers />
+
+
+      <Footer />
 
     </>
   );
