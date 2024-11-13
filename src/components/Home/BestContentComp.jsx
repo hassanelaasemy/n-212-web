64182988f0a7befd5fbc189ee45d140fe5cea0e4
khalidmk1 <khalidmk1@users.noreply.github.com>
2024-11-13 16:14:45 +0100

    Your commit last here

--- src/components/Home/BestContentComp.jsx
+++ src/components/Home/BestContentComp.jsx
@@ -7,13 +7,10 @@ import { Pagination } from "swiper/modules";
 import ContentApi from "../../../Api/ContentApis";
 import { useQuery } from "@tanstack/react-query";
 import { BASE_URL } from "../../../config";
+import { COLORS } from "../../../constant/theme";
 
 export default function BestContentComp() {
-
-    const {
-        data: items,
-        isLoading,
-    } = useQuery({
+    const { data: chains, isLoading } = useQuery({
         queryKey: ["youtubData"],
         queryFn: ContentApi.getYoutubeContent,
         throwOnError: (error) => {
@@ -39,37 +36,60 @@ export default function BestContentComp() {
             }}
             modules={[Pagination]}
             className="mySwiper"
-            style={{ height: '474px' }}
+            style={{ height: "474px" }}
         >
-            {items.map((slide, index) => (
+            {chains.map((chain, index) => (
                 <SwiperSlide key={index}>
-
-
                     <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
                         <a href="#">
-                            <img className="rounded-t-lg w-full" style={{ height: '215px' }} src={BASE_URL + `/storage/` + slide.image} alt={slide.title} />
-
+                            <img
+                                className="rounded-t-lg w-full"
+                                style={{ height: "215px" }}
+                                src={BASE_URL + `/storage/` + chain.image}
+                                alt={chain.title}
+                            />
                         </a>
-                        <div class="p-5">
-                            <a href="#">
-                                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                            </a>
-                            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-                            <a href="#" class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
-                                </svg>
-                            </a>
+                        <div className="p-4 ml-auto">
+                            <h6 className="mb-2 text-slate-800 text-xl font-semibold text-end">
+                                {chain.title}
+                            </h6>
+
+                            <div className="p-3 flex flex-wrap gap-2 ml-auto ">
+                                <span
+                                    style={{
+                                        backgroundColor: COLORS.primarygreen,
+                                    }}
+                                    className={`text-xs font-medium px-2.5 py-0.5 rounded text-white ml-auto`}
+                                >
+                                    {chain.categoryName}
+                                </span>
+                            </div>
+
+                            <p className="text-slate-600 leading-normal font-light ml-auto">
+                                {"..." + chain.smallDescription.substring(0, 50)}
+                            </p>
+                        </div>
+                        <div className="flex items-center justify-between p-4">
+                            <div className="flex items-center">
+                                {chain.user.avatar && (
+                                    <img
+                                        alt={chain?.user?.fullName}
+                                        src={BASE_URL + "/storage/" + chain?.user?.avatar}
+                                        className="relative inline-block h-8 w-8 rounded-full"
+                                    />
+                                )}
+
+                                <div className="flex flex-col ml-3 text-sm">
+                                    <span className="text-slate-800 font-semibold">
+                                        {chain?.user?.fullName}
+                                    </span>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
-
-
                 </SwiperSlide>
             ))}
         </Swiper>
-
-    )
- 
-  
+    );
 }
