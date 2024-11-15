36f01bb4779a6aebf53cfa87e1dcdbbf3bff6db8
khalidmk1 <khalidmk1@users.noreply.github.com>
2024-11-15 12:17:43 +0100

    handle the layoute

--- redux/store.js
+++ redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeSlice from "./ThemeSlice";
 import authSlice from "./authSlice";
-import UserSlice from "./UserSlice";
+import UserSlice from './userSlice'
 export const store = configureStore({
   reducer: {
     theme: themeSlice,
--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,36 +1,40 @@
+import React from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+import GuestLayout from "./Layout/GuestLayout";
+import Authenticated from "./Layout/Authenticated";
+
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Auth/Register";
-import { useSelector } from "react-redux";
-import Authenticated from "./Layout/Authenticated";
 
 export default function AppRoutes() {
   const loggedIn = useSelector((state) => state.auth.isAuthenticated);
 
   return (
     <Router>
-      <Authenticated >
-        <Routes>
-          <Route
-            path="/"
-            element={loggedIn ? <Home /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="/login"
-            element={!loggedIn ? <Login /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/register"
-            element={!loggedIn ? <Register /> : <Navigate to="/" />}
-          />
-        </Routes>
-      </Authenticated>
+      {loggedIn ? (
+        <Authenticated>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </Authenticated>
+      ) : (
+        <GuestLayout>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Navigate to="/login" />} />
+          </Routes>
+        </GuestLayout>
+      )}
     </Router>
   );
 }
