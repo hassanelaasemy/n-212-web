d58cfd145519ed0137c528ddaf21bf719c6c5cad
khalidmk1 <khalidmk1@users.noreply.github.com>
2024-11-25 11:22:35 +0100

    handle the cours detail

--- src/components/Cours/CoursVideoDetail.jsx
+++ src/components/Cours/CoursVideoDetail.jsx
@@ -37,7 +37,7 @@ export default function CoursVideoDetail({ content }) {
 
             {videos.map((video) => (
                 <div
-                    key={video.id}
+                    key={'video-content-'+video.id}
                     className="max-w-sm mt-2 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
                 >
                     <a href="#">
--- src/pages/Cours/DetailCours.jsx
+++ src/pages/Cours/DetailCours.jsx
@@ -7,6 +7,7 @@ import CommentCours from "../../components/Cours/CommentCours";
 import UserInfo from "../../components/Cours/UserInfo";
 import CoursVideoDetail from "../../components/Cours/CoursVideoDetail";
 import '../../css/detailCours.css'
+import ReactPlayer from "react-player";
 
 export default function DetailCours() {
     const { id } = useParams();
@@ -24,6 +25,9 @@ export default function DetailCours() {
 
     if (isLoading) return <div>Loading...</div>;
 
+    console.log(content);
+
+
     return (
 
         <div className="grid grid-cols-1 sm:grid-cols-12 gap-2">
@@ -33,9 +37,29 @@ export default function DetailCours() {
             <div className="col-span-12 sm:col-span-8">
 
                 <div className="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-                    <img className="p-3 rounded-t-lg" src={`${BASE_URL}/storage/${content?.image}`} alt="product image" />
+                    {content.contentType === 'formation'
+                        ? <img className="p-3 rounded-t-lg" src={`${BASE_URL}/storage/${content?.image}`} alt="product image" />
+                        : <div className="md:h-[300px] lg:h-[500px]">
+                            <ReactPlayer config={{
+                                youtube: {
+                                    playerVars: {
+                                        controls: 1,
+                                        modestbranding: 1,
+                                        rel: 0,
+                                        iv_load_policy: 3,
+                                    },
+                                },
+                            }}
+                                width="100%"
+                                height="100%"
+                                style={{ borderRadius: '20px' }}
+                                controls={true}
+                                playing={false}
+                                url={content.video} />
+                        </div>}
+
                     <h5 className="mb-2 text-3xl font-bold text-gray-900 dark:text-white">{content.title}</h5>
-                    <p className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">{content.smallDescription}</p>
+                    <div className="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400" dangerouslySetInnerHTML={{ __html: content.smallDescription }} />
 
                 </div>
 
@@ -46,7 +70,7 @@ export default function DetailCours() {
 
             </div>
             <div className="col-span-12 w-full sm:col-span-4 comment_cours">
-               
+
 
             </div>
         </div>
